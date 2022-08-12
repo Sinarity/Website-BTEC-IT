6a0c12ee8830c4a0ca84e5fa8872be993309a688
Sinarity <44562053+Sinarity@users.noreply.github.com>
2022-08-12 13:10:06 +0400

    Removed broken javascript

--- ttest.js
+++ ttest.js
@@ -3,5 +3,4 @@
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  scrollTo(0, 8000); // it will take 8 seconds to reach to top.
 }
